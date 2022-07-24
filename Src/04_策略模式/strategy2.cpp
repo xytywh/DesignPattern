53759cf3334422b115c63b633d3340c68c11d9d0
gehui <15555387016gehui@gmail.com>
2022-07-24 17:14:34 +0800

    update 策略模式

--- Src/04_策略模式/strategy2.cpp
+++ Src/04_策略模式/strategy2.cpp
@@ -34,7 +34,8 @@ public:
     }
 };
 
-//扩展
+//扩展  遵循了开闭原则 这里是一个新的类  一般按照C++的编码规范 我们写到一个新的文件中  比如编译器支持增量编译  增量部署之类的
+// 就会比strategy1写法节省很多时间
 //*********************************
 class FRTax : public TaxStrategy
 {
@@ -51,6 +52,7 @@ private:
     TaxStrategy *strategy;//基类指针，用于多态调用
 
 public:
+    // 这里是一个工厂模式  可以返回不同的之类指针
     SalesOrder(StrategyFactory *strategyFactory)
     {//赋值具体的子类对象
         this->strategy = strategyFactory->NewStrategy();
