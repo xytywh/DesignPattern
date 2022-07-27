ba8585f13604fadda2590cca678fa786bb2d0c7a
gehui <15555387016gehui@gmail.com>
2022-07-27 22:21:29 +0800

    update 工厂方法

--- Src/08_工厂模式/MainForm1.cpp
+++ Src/08_工厂模式/MainForm1.cpp
@@ -8,6 +8,8 @@ public:
     void Button1_Click()
     {
 
+        // 面向接口编程  典型表现就是变量要申明成抽象基类
+        // 这里的new的BinarySplitter也违背了依赖倒置原则 也依赖BinarySpitter存在此文件才能编译
         ISplitter *splitter = new BinarySplitter(); //依赖具体类
 
         splitter->split();
--- Src/08_工厂模式/MainForm2.cpp
+++ Src/08_工厂模式/MainForm2.cpp
@@ -1,5 +1,7 @@
+// MainForm2本身不依赖于具体类了 我们把它对具体类的依赖给提出去了 是的其本身不依赖于具体类了
 class MainForm : public Form
 {
+    // 这里是抽象基类 不依赖具体类
     SplitterFactory *factory; //工厂
 
 public:
@@ -10,6 +12,7 @@ public:
 
     void Button1_Click()
     {
+        // 这里是抽象基类 不依赖具体类
         ISplitter *splitter = factory->CreateSplitter(); //多态new
 
         splitter->split();
