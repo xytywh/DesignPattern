eec01a1476632f3fc15442d16b1b3b5fc479eb21
gehui <15555387016gehui@gmail.com>
2022-07-29 22:06:15 +0800

    update 原型模式

--- Src/10_原型模式/ConcretePrototype.cpp
+++ Src/10_原型模式/ConcretePrototype.cpp
@@ -1,36 +1,30 @@
-//具体类
-class BinarySplitter : public ISplitter
-{
+// 具体类
+// 和工厂模式的不同之处在于省去了用于生成各种不同对象的继承了工厂基类的具体工厂类（这个生成对象的工作在自己的类对象加了一个clone函数完成了）
+// 成员函数clone完成了生成自己的任务
+class BinarySplitter : public ISplitter {
 public:
-    virtual ISplitter *clone()
-    {
-        return new BinarySplitter(*this);//需要拷贝构造函数正确实现
+    virtual ISplitter *clone() {
+        return new BinarySplitter(*this); // 需要拷贝构造函数正确实现
     }
 };
 
-class TxtSplitter : public ISplitter
-{
+class TxtSplitter : public ISplitter {
 public:
-    virtual ISplitter *clone()
-    {
+    virtual ISplitter *clone() {
         return new TxtSplitter(*this);
     }
 };
 
-class PictureSplitter : public ISplitter
-{
+class PictureSplitter : public ISplitter {
 public:
-    virtual ISplitter *clone()
-    {
+    virtual ISplitter *clone() {
         return new PictureSplitter(*this);
     }
 };
 
-class VideoSplitter : public ISplitter
-{
+class VideoSplitter : public ISplitter {
 public:
-    virtual ISplitter *clone()
-    {
+    virtual ISplitter *clone() {
         return new VideoSplitter(*this);
     }
 };
