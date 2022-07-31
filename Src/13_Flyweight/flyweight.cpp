1993e5efa39fd2cae9ae6c765f281fea7c00b492
gehui <15555387016gehui@gmail.com>
2022-07-31 16:23:08 +0800

    享元模式

--- Src/13_Flyweight/flyweight.cpp
+++ Src/13_Flyweight/flyweight.cpp
@@ -1,5 +1,4 @@
-class Font
-{
+class Font {
 private:
     //unique object key，用于查找指定对象
     string key;
@@ -7,36 +6,31 @@ private:
     //....
 
 public:
-    Font(const string &key)
-    {
+    Font(const string &key) {
         //...
     }
 };
 
-class FontFactory
-{
+class FontFactory {
 private:
     map<string, Font *> fontPool; //对象池
 
 public:
-    Font *GetFont(const string &key)
-    {
+    Font *GetFont(const string &key) {
         map<string, Font *>::iterator item = fontPool.find(key);
 
-        if (item != footPool.end())
-        {
+        // 同一个key  只会在系统中有一个对象  不会出现对象数量太多的问题
+        // 对象池实现对象的共享
+        if (item != footPool.end()) {
             return fontPool[key];//对象池存在，直接返回
-        }
-        else
-        {//找不到对象，创建新对象返回
+        } else {//找不到对象，创建新对象返回
             Font *font = new Font(key);
             fontPool[key] = font;
             return font;
         }
     }
 
-    void clear()
-    {
+    void clear() {
         //...
     }
 };
