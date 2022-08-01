33b25f3e19829325945062335f47b3c6e2a9f65e
gehui <15555387016gehui@gmail.com>
2022-08-01 22:53:32 +0800

    代理模式

--- Src/15_Proxy/client.cpp
+++ Src/15_Proxy/client.cpp
@@ -1,33 +1,29 @@
-class ISubject
-{
+class ISubject {
 public:
     virtual void process();
 };
 
-class RealSubject : public ISubject
-{
+class RealSubject : public ISubject {
 public:
-    virtual void process()
-    {
-        //....
+    virtual void process() {
+        // ....
     }
 };
 
-class ClientApp
-{
+class ClientApp {
 private:
     ISubject *subject;
 
 public:
-    ClientApp()
-    {
+    ClientApp() {
+        // 这里表示的含义并不一定代表new一个RealSubject对象  而是指通过某种方法返回一个RealSubject对象
+        // 但是存在这么一种情况 比如分布式或者安全控制的原因 我们 不能直接得到这个RealSubject对象
         subject = new RealSubject();
     }
 
-    void DoTask()
-    {
-        //...
+    void DoTask() {
+        // ...
         subject->process();
-        //....
+        // ....
     }
 };
--- Src/15_Proxy/proxy.cpp
+++ Src/15_Proxy/proxy.cpp
@@ -1,35 +1,30 @@
-class ISubject
-{
+class ISubject {
 public:
     virtual void process();
 };
 
-//Proxy的设计
-class SubjectProxy : public ISubject
-{
+// Proxy的设计
+class SubjectProxy : public ISubject {
 public:
-    virtual void process()
-    {
-        //对RealSubject的一种间接访问
-        //....很多操作
+    virtual void process() {
+        // 对RealSubject的一种间接访问
+        // ....很多操作
+        // 实现的差异可能非常之大
     }
 };
 
-class ClientApp
-{
+class ClientApp {
 private:
     ISubject *subject;
 
 public:
-    ClientApp()
-    {
+    ClientApp() {
         subject = new SubjectProxy();
     }
 
-    void DoTask()
-    {
-        //...
+    void DoTask() {
+        // ...
         subject->process();
-        //....
+        // ....
     }
 };
